06026e9bcc36a2500f313acc07d3e730bc64446d
Ovy95 <jackoverton21@hotmail.com>
2020-10-26 10:25:04 +0000

    Refactored Raindrops

--- raindrops/go.mod
+++ raindrops/go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/tools v0.0.0-20200831203904-5a2aa26beb65 // indirect
+	golang.org/x/tools v0.0.0-20201023174141-c8cfbd0f21e6 // indirect
 )
--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,31 +1,29 @@
 package raindrops
 
-import "strconv"
+import (
+	"strconv"
+)
 
 func Convert(i int) string {
 
 	// Created remainder as feel its easier to read, Bit unnecessary, feel it reads easier
 	remainder := 0
-	// checks each that int that gets passed is divisible by more
-	//then one value before singler to get the right result
+	word := ""
 
-	if i%3 == remainder && i%5 == remainder && i%7 == remainder {
-		return "PlingPlangPlong"
-	} else if i%5 == remainder && i%7 == remainder {
-		return "PlangPlong"
-	} else if i%3 == remainder && i%7 == remainder {
-		return "PlingPlong"
-	} else if i%3 == remainder && i%5 == remainder {
-		return "PlingPlang"
-	} else if i%3 == remainder {
-		return "Pling"
-	} else if i%5 == remainder {
-		return "Plang"
-	} else if i%7 == remainder {
-		return "Plong"
-		// else uses strcov package to convert int to string
-	} else {
-		return strconv.Itoa(i)
+	// Word is a empty string ready to get appended values if is divisible by the int
+	if i%3 == remainder {
+		word += "Pling"
 	}
+	if i%5 == remainder {
+		word += "Plang"
+	}
+	if i%7 == remainder {
+		word += "Plong"
+	}
+	if i%3 != remainder && i%5 != remainder && i%7 != remainder {
+		return strconv.Itoa(i)
+		// Uses strcov package to convert int to string returns just value
+	}
+	return word
 
 }
