210dcc7d5aea70d8eb16e45ba4e80788ff466501
Ovy95 <jackoverton21@hotmail.com>
2020-10-27 09:23:48 +0000

    Refactored

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -2,28 +2,29 @@ package raindrops
 
 import (
 	"strconv"
+	"strings"
 )
 
+// Convert Takes in a int value returns a string value depending on the int value given.
 func Convert(i int) string {
-
-	// Created remainder as feel its easier to read, Bit unnecessary, feel it reads easier
-	remainder := 0
 	word := ""
-
 	// Word is a empty string ready to get appended values if is divisible by the int
-	if i%3 == remainder {
+
+	if i%3 == 0 {
 		word += "Pling"
 	}
-	if i%5 == remainder {
+	if i%5 == 0 {
 		word += "Plang"
 	}
-	if i%7 == remainder {
+	if i%7 == 0 {
 		word += "Plong"
 	}
-	if i%3 != remainder && i%5 != remainder && i%7 != remainder {
-		return strconv.Itoa(i)
-		// Uses strcov package to convert int to string returns just value
+	/*ContainsAny Function checks value inside word to check if it matches
+	  if true returns word value.
+	*/
+	if strings.ContainsAny("PlingPlangPlong", word) {
+		return word
 	}
-	return word
 
+	return strconv.Itoa(i)
 }
